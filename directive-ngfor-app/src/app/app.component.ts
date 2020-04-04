fa83cf56e19e8652d1547f232878bfb6a437c1a8
gottamsnehamary <61041030+gottamsnehamary@users.noreply.github.com>
2020-04-04 21:38:42 +0530

    directives

---
+++ directive-ngfor-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  productList : Array<string> = [
+    'phone',
+    'Galaxy 9.0',
+    'BlackBerrry 10z',
+    'Mi'
+  ]
+}
